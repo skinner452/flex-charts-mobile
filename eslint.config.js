2102f6b91034b279a65494062ca85432efc5dd79
Alec Skinner <skinner452@gmail.com>
2025-01-27 16:18:00 -0500

    Added eslint

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import pluginJs from "@eslint/js";
+import pluginTs from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactNative from "eslint-plugin-react-native";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...pluginTs.configs.recommended,
+  {
+    plugins: {
+      "react-native": pluginReactNative,
+    },
+    rules: pluginReactNative.configs.all.rules,
+  },
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+    },
+    rules: pluginReactHooks.configs.recommended.rules,
+  },
+];
